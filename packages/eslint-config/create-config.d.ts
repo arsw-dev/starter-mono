e43c9dec8271fe1a9f722e34c13e740861096f21
Aryan Swaminath <me@arsw.dev>
2026-01-09 13:43:51 -0600

    feat: add linter rule to disallow export const, export func, and export default func

--- packages/eslint-config/create-config.d.ts
+++ packages/eslint-config/create-config.d.ts
@@ -4,4 +4,5 @@ type AntfuParams = Parameters<typeof antfu>;
 type AntfuReturn = ReturnType<typeof antfu>;
 type Options = AntfuParams[0];
 type UserConfigs = AntfuParams[1][];
-export const createConfig: (options?: Options, ...userConfigs: UserConfigs) => AntfuReturn;
+const createConfig: (options?: Options, ...userConfigs: UserConfigs) => AntfuReturn;
+export { createConfig };
